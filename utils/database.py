cfa6fb808b317d9ec4c4eecfc8463bed03ac32e3
Culer <culer84@inbox.ru>
2021-06-08 17:04:51 +0300

    Initial project

--- utils/database.py
+++ utils/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:B@nk_pa55@192.168.56.3/fastapi_test"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:B@nk_pa55@192.168.56.3/fastapi_test"
+SQLALCHEMY_DATABASE_URL = "postgres://ussdzjzoipnsdc:ef296b13fd776a54458f5fac8469fad3004e962f80cb0e72c0a2451f369a37f3@ec2-63-33-239-176.eu-west-1.compute.amazonaws.com:5432/d4ckhtoedpsmmp"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
